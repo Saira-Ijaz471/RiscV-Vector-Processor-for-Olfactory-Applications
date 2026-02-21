d4efa4f9836bc9fe79ce14bc5724557e3c0931ac
Saira-Ijaz471 <135571961+Saira-Ijaz471@users.noreply.github.com>
2026-02-21 15:13:54 +0500

    Delete host_train directory

--- host_train/generated/model_scaler_var.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-static const double scaler_var[6] = {
-    1369.52100034,158.492940448,2023.89055733,19590.8022418,186.917359618,1537.6679279
-};
-static const int scaler_var_size = 6;
